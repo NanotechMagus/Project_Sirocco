2a1637a49e7f0a41061d7b4bedf50a4b5a45d7e0
NanotechMagus <bfrostbourne@gmail.com>
2019-09-30 09:31:23 -0400

    Initialized Google Calendars module

---
+++ core/google.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3.7
+
+# Standard Library Imports
+import datetime
+import pickle
+import os.path
+
+# Locally Developed Imports
+
+# Third Party Imports
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+
+
+class gcal:
+
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+
+    def __init__(self):
+        self.SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+        self.__creds = None
+        if os.path.exists('./conf/token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                self.__creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.__creds or not self.__creds.valid:
+            if self.__creds and self.__creds.expired and self.__creds.refresh_token:
+                self.__creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    './conf/credentials.json', self.SCOPES)
+                self.__creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(self.__creds, token)
+        self.service = build('calendar', 'v3', credentials=self.__creds)
+
+    def get_cal(self):
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = self.service.events().list(
+            calendarId='primary',
+            timeMin=now,
+            maxResults=10,
+            singleEvents=True,
+            orderBy='startTime'
+        ).execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
