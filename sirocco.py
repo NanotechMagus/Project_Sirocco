3fcade2d5cd2cc8a8090ecfc25b729f2fbcfda1a
NanotechMagus <bfrostbourne@gmail.com>
2019-10-15 09:49:57 -0400

    ~Fixed extensions load

--- sirocco.py
+++ sirocco.py
@@ -5,51 +5,39 @@ import logging
 import os
 
 # Locally Developed Imports
+from core.core import initialize
 
 # Third Party Imports
 from discord.ext import commands
 
-# Initial variables
-disconf = {}
-basedir = ""
-
-
-def set_init():
-    from core.core import fileHandler
-    global basedir
-    global disconf
-
-    conffile = fileHandler('conf', 'config.json')
-    disconf = conffile.get_data('Discord')
-    basedir = conffile.basePath
+log = logging.basicConfig()
 
 
 def main():
 
-    set_init()
+    init = initialize()
 
-    sirocco = commands.Bot(command_prefix=disconf['PREFIX'], description=disconf['DESCRIPTION'])
+    sirocco = commands.Bot(command_prefix=init.disconf['PREFIX'], description=init.disconf['DESCRIPTION'])
 
-    '''
-    for extension in gather_extensions():
+    for extension in gather_extensions(init.cogdir):
         try:
+            print(f'{extension}')
             sirocco.load_extension(extension)
         except FileNotFoundError as err:
             logging.warning(f'Failed to load extension {extension}.')
-    '''
 
-    sirocco.run(disconf['TOKEN'], bot=True)
+    sirocco.run(init.disconf['TOKEN'], bot=True)
 
     return
 
 
-def gather_extensions():
+def gather_extensions(basedir):
 
     cogsdir = os.path.join(basedir, "cogs")
     reglist = [f for f in os.listdir(cogsdir) if os.path.isfile(os.path.join(cogsdir, f))]
     splitlist = [os.path.splitext(x)[0] for x in reglist]
 
-    return ["cogs." + x for x in splitlist]
+    return ["core.cogs." + x for x in splitlist]
 
 
 if __name__ == "__main__":
