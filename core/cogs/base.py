f424eee8d39d190b29accf022641d1662b20aa85
NanotechMagus <bfrostbourne@gmail.com>
2019-10-15 09:50:31 -0400

    ~Ping command added

---
+++ core/cogs/base.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+# Standard Library Imports
+import random
+import string
+import json
+from datetime import datetime as d
+
+# Third Party Imports
+import discord
+from discord.ext import commands
+from urllib.parse import urljoin
+
+# Locally Developed Imports
+
+
+class base(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(
+        name='ping',
+        description='The ping command',
+        aliases=['p']
+    )
+    async def ping_command(self, ctx):
+        start = d.timestamp(d.now())
+        # Gets the timestamp when the command was used
+
+        msg = await ctx.send(content='Pinging')
+        # Sends a message to the user in the channel the message with the command was received.
+        # Notifies the user that pinging has started
+
+        await msg.edit(content=f'Pong!\nOne message round-trip took {(d.timestamp(d.now()) - start) * 1000}ms.')
+        # Ping completed and round-trip duration show in ms
+        # Since it takes a while to send the messages, it will calculate how much time it takes to edit an message.
+        # It depends usually on your internet connection speed
+
+        return
+
+
+def setup(client):
+    client.add_cog(base(client))
